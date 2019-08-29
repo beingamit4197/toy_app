16ae381d733343cd64e47fee0ea62a356f22a421
Amit Rawat <beingamit4197@gmail.com>
2019-08-29 12:10:38 +0530

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-  def hello
+  def hello 
+    @title= "Home"
     render html: "Hello! Amit"
   end
 end
